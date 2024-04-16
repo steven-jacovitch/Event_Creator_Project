38b6ba93eb113d0967cf8ffc13dc6fd093c95c13
Steven Jacovitch <sjacovit@umich.edu>
2024-04-15 23:39:51 -0400

    modified CountdownTimer component so that when time expires, a message informing user that event has passed appears instead of the timer

--- src/assets/components/CountdownTimer.jsx
+++ src/assets/components/CountdownTimer.jsx
@@ -1,43 +1,49 @@
 import React, { useState, useEffect } from 'react';
 import './style.css';
 
-
 const calculateTimeLeft = (countdownTarget) => {
-    const totalTimeLeft = countdownTarget - new Date();
-    const days = Math.floor(totalTimeLeft / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((totalTimeLeft / (1000 * 60 * 60)) % 24);
-    const minutes = Math.floor((totalTimeLeft / 1000 / 60) % 60);
-    const seconds = Math.floor((totalTimeLeft / 1000) % 60);
-    return [{ label: 'days', value: days }, { label: 'hours', value: hours }, { label: 'minutes', value: minutes }, { label: 'seconds', value: seconds }];
+  const totalTimeLeft = countdownTarget - new Date();
+  if (totalTimeLeft < 0) {
+    return null;
+  }
+  const days = Math.floor(totalTimeLeft / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((totalTimeLeft / (1000 * 60 * 60)) % 24);
+  const minutes = Math.floor((totalTimeLeft / 1000 / 60) % 60);
+  const seconds = Math.floor((totalTimeLeft / 1000) % 60);
+  return [{ label: 'Days', value: days }, { label: 'Hours', value: hours }, { label: 'Minutes', value: minutes }, { label: 'Seconds', value: seconds }];
 }
 
 const CountdownTimer = ({ targetDate }) => {
-    const countdownTarget = new Date(targetDate);
-    const [timeLeft, setTimeLeft] = useState(() => calculateTimeLeft(countdownTarget));
+  const countdownTarget = new Date(targetDate);
+  const [timeLeft, setTimeLeft] = useState(() => calculateTimeLeft(countdownTarget));
 
-    useEffect(() => {
-        const timer = setInterval(() => {
-            setTimeLeft(calculateTimeLeft(countdownTarget));
-        }, 1000)
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft(calculateTimeLeft(countdownTarget));
+    }, 1000)
 
-        return () => clearInterval(timer);
-    }, [countdownTarget]);
+    return () => clearInterval(timer);
+  }, [countdownTarget]);
 
-    return (
-        <div className="countdown">
-            <h3>Time Remaining</h3>
-            <div className="content">
-                {timeLeft.map(item => (
-                    <div className="box" key={item.label}>
-                        <div className="value">
-                            <span>{item.value}</span>
-                        </div>
-                        <span className="label">{item.label}</span>
-                    </div>
-                ))}
+  if (!timeLeft) {
+    return <p>The event has passed</p>;
+  }
+
+  return (
+    <div className="countdown">
+      <h3>Time Remaining</h3>
+      <div className="content">
+        {timeLeft.map(item => (
+          <div className="box" key={item.label}>
+            <div className="value">
+              <span>{item.value}</span>
             </div>
-        </div>
-    )
+            <span className="label">{item.label}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default CountdownTimer;
--- src/assets/components/Event.jsx
+++ src/assets/components/Event.jsx
@@ -3,15 +3,16 @@ import CountdownTimer from './CountdownTimer';
 
 // this component will be used to display the event name and the countdown timer as well as a button to remove the event
 
+
 const Event = ({ eventName, targetDate, onRemove }) => {
     return (
-                <div className="event">
-                    <h2>{eventName}</h2>
-                    <CountdownTimer targetDate={targetDate} />
-                    <div className="button-container">
-                        <button className="button-64" role="button" onClick={onRemove}><span className="text">Remove Event</span></button>
-                    </div>
-                </div>
+        <div className="event">
+            <h2>{eventName}</h2>
+            <CountdownTimer targetDate={targetDate} />
+            <div className="button-container">
+                <button className="button-64" role="button" onClick={onRemove}><span className="text">Remove Event</span></button>
+            </div>
+        </div>
     )
 }
 
